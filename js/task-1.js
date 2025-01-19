9e4b3a6bfe6f43149a672992d9509d39549f63fe
AlexP <alexperepelyuk@gmail.com>
2025-01-19 14:21:21 +0100

    add task-1 and task-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,33 +1,13 @@
-const customer = {
-    username: 'Mango',
-    balance: 24000,
-    discount: 0.1,
-    orders: ['Burger', 'Pizza', 'Salad'],
+const ulElem = document.querySelectorAll('ul#categories > li.item');
+console.log(`Number of categories: ${ulElem.length}`);
 
-    getBalance() {
-        return this.balance;
-    },
+const ulCategories = document.querySelector('ul#categories');
+const items = ulCategories.querySelectorAll('li.item');
 
-    getDiscount() {
-        return this.discount;
-    },
+items.forEach(item => {
+    const title = item.querySelector('h2').textContent;
+    const itemCount = item.querySelectorAll('li').length;
 
-    setDiscount(value) {
-        this.discount = value;
-    },
-
-    getOrders() {
-        return this.orders;
-    },
-
-    addOrder(cost, order) {
-        this.balance -= cost - cost * this.discount;
-        this.orders.push(order);
-    },
-};
-
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+    console.log(`Category: ${title}`);
+    console.log(`Number of items: ${itemCount}`);
+});
